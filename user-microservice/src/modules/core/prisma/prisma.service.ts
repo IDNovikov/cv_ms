f275540022164425ace69d0358a75a641c32d016
Ilya <affective.neurosis@gmail.com>
2026-02-17 12:14:30 +0300

    REF: module intialization

--- user-microservice/src/modules/core/prisma/prisma.service.ts
+++ user-microservice/src/modules/core/prisma/prisma.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from 'prisma/generated/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  private readonly logger = new Logger(PrismaService.name);
   constructor() {
     const adapter = new PrismaPg({
       connectionString: process.env.DATABASE_URL!,
@@ -14,8 +23,23 @@ export class PrismaService extends PrismaClient {
       log: ['warn', 'error'],
     });
   }
+  async onModuleInit() {
+    this.logger.log('Connection to db');
+    try {
+      await this.$connect();
+      this.logger.log('Db is connected');
+    } catch (err) {
+      this.logger.error(err);
+    }
+  }
 
-  // async transaction<T>(fn: (tx: Prisma.TransactionClient) => Promise<T>) {
-  //   return this.$transaction((tx) => fn(tx));
-  // }
+  async onModuleDestroy() {
+    this.logger.log('Disconnect from db');
+    try {
+      await this.$disconnect();
+      this.logger.log('Db is disconnected');
+    } catch (err) {
+      this.logger.error(err);
+    }
+  }
 }

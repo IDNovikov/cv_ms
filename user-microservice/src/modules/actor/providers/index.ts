b7c52ba116d05c6f07eedc7981a4b8be12e54113
Ilya <affective.neurosis@gmail.com>
2026-02-17 12:15:06 +0300

    ADD index

--- user-microservice/src/modules/actor/providers/index.ts
+++ user-microservice/src/modules/actor/providers/index.ts
@@ -1,3 +1,7 @@
 export * from './prisma/prisma.port';
+export * from './prisma/prisma.adapter';
 export * from './redis/redis.port';
+export * from './redis/redis.adapter';
 export * from './amqp/amqp.port';
+export * from './amqp/amqp.adapter';
+export * from './actor-facade.factory';
